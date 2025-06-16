84ab5afa220e4fa19d2ef2f43bc11066ebc40e6f
JesuisDv <jdfrancov2020@gmail.com>
2025-06-16 02:04:07 -0500

    Mejorar la navegación y la estructura del sitio; agregar formulario de contacto y slider de películas en la página de cartelera; actualizar estilos y diseño responsivo.

--- scripts.js
+++ scripts.js
@@ -1,24 +1,24 @@
-const API_KEY = '58765c04fe038d73593baffdc58ea6a4'; 
-const BASE_URL = 'https://api.themoviedb.org/3';
-const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+const API_KEY = "58765c04fe038d73593baffdc58ea6a4";
+const BASE_URL = "https://api.themoviedb.org/3";
+const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
 async function cargarPeliculas() {
-  try {
-    const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=es-ES&page=1`);
+  try{
+    const res = await fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=es-ES&page=1`);
     const data = await res.json();
-    mostrarPeliculas(data.results);
-  } catch (error) {
-    console.error('Error al cargar películas:', error);
+    mostrarPeliculas(data.results)
+  } catch (error){
+    console.error("Error al cargar peliculas: ", error)
   }
 }
 
-function mostrarPeliculas(peliculas) {
-  const contenedor = document.getElementById('peliculas');
-  contenedor.innerHTML = '';
+function mostrarPeliculas(peliculas){
+  const contenedor = document.getElementById("peliculas");
+  contenedor.innerHTML = "";
 
   peliculas.forEach(pelicula => {
-    const div = document.createElement('div');
-    div.classList.add('pelicula');
+    const div = document.createElement("div");
+    div.classList.add("pelicula")
     div.innerHTML = `
       <img src="${IMG_BASE_URL + pelicula.poster_path}" alt="${pelicula.title}">
       <h3>${pelicula.title}</h3>
@@ -28,7 +28,36 @@ function mostrarPeliculas(peliculas) {
   });
 }
 
-
 if (window.location.pathname.includes('cartelera.html')) {
   cargarPeliculas();
 }
+
+// Slider Home !
+
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = document.getElementById('sliderCartelera');
+
+  document.querySelector('.previaSlider').addEventListener('wheel', function (e) {
+    e.preventDefault();
+    this.scrollLeft += e.deltaY;
+  });
+
+  fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=es-ES`)
+    .then(res => res.json())
+    .then(data => {
+      const peliculas = data.results.slice(0, 10); // carga 10 para que haya scroll visible
+      peliculas.forEach(p => {
+        const div = document.createElement('div');
+        div.classList.add('slide');
+        div.innerHTML = `
+          <img src="https://image.tmdb.org/t/p/w342${p.poster_path}" alt="${p.title}">
+          <div class="info">
+            <h3>${p.title}</h3>
+          </div>
+        `;
+        slider.appendChild(div);
+      });
+    })
+    .catch(err => console.error('Error al cargar películas:', err));
+});
+
